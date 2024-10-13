01f3420010653bfa4e417b58ad2a310d927f3c74
Developer.WK <166378578+DeveloperWK@users.noreply.github.com>
2024-10-13 21:32:17 +0600

    Delete Scoped CSS Variables and JS directory

--- Scoped CSS Variables and JS/main.js
+++
@@ -1,8 +0,0 @@
-var inputs = document.querySelectorAll(".controls input");
-function handleUpdate() {
-    var suffix = this.dataset.sizing || "";
-    document.documentElement.style.setProperty("--".concat(this.name), this.value + suffix);
-    console.log(suffix);
-}
-inputs.forEach(function (input) { return input.addEventListener("change", handleUpdate); });
-inputs.forEach(function (input) { return input.addEventListener("mousemove", handleUpdate); });
